81fc80168525883bacc3ac894ac0cb05ed0eba3b
Esmeraldaagb <esmeraldaagbessy18@gmail.com>
2025-05-24 14:06:37 +0100

    Initial commit from Create Next App

---
+++ src/lib/constants/index.ts
@@ -0,0 +1,4 @@
+export const APP_NAME= process.env.NEXT_PUBLIC_APP_NAME || 'SAS';
+export const APP_DESCRIPTION= process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Développement d’une plateforme hybride proposant à la fois des espaces à la demande et des expériences premium";
+
+export const SERVER_URL=process.env.SERVER_URL || 'https://localhost:3000' 
